c917b6d10df8360f78f5664115ae37d810e0bb01
GyanaprakasKhandual <gyanaprakashkhnadual@gmail.com>
2026-04-17 10:28:05 +0530

    Conditions in Java intitial commit

---
+++ chapters/basics/Conditions.java
@@ -0,0 +1,80 @@
+
+/*
+Conditional Statements in Java Language
+- If Statement
+- If Else Statement
+- Switch Statement
+*/
+
+public class Conditions {
+
+    public static void main(String[] args) {
+
+        /*
+         * Scanner sc = new Scanner(System.in);
+         * int age = sc.nextInt();
+         * int num = sc.nextInt();
+         * int a = sc.nextInt();
+         * int b = sc.nextInt();
+         * int button = sc.nextInt();
+         */
+        /*
+         * if (age > 18) {
+         * System.out.println("You are an Adult");
+         * } else {
+         * System.out.println("You are not an Adult");
+         * }
+         */
+
+        /*
+         * if ( num % 2 == 0) {
+         * System.out.println("Even");
+         * }
+         * else {
+         * System.out.println("Odd");
+         * }
+         */
+
+        /*
+         * if (a == b) {
+         * System.out.println("Equal");
+         * } else if (a > b) {
+         * System.out.println("A is grated than B");
+         * } else {
+         * System.out.println("A is lesser that B");
+         * }
+         */
+
+        /*
+         * if ( button == 1 ) {
+         * System.out.print("Hello");
+         * }
+         * else if ( button == 2 ) {
+         * System.out.println("Namaste");
+         * }
+         * else if ( button == 3) {
+         * System.out.println("Bonjur");
+         * }
+         * else {
+         * System.out.println("You are clicked and invalid number");
+         * }
+         */
+
+        /*
+         * switch (button) {
+         * 
+         * case 1:
+         * System.err.println("Hello");
+         * break;
+         * case 2:
+         * System.err.println("Namaste");
+         * break;
+         * case 3:
+         * System.err.println("Bonjur");
+         * break;
+         * default:
+         * System.err.println("Invalid Button");
+         * }
+         */
+    }
+}
